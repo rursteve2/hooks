5166c47073ba5d02d0fa0db3adc0f3b97e64e143
Stephen <rursteve2@yahoo.com>
2020-01-17 16:55:59 -0500

    mouse movement useeffect hook

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,35 @@ import './App.css';
 
 function App() {
   const [num, setNum] = useState(0)
+  const [xCoord, setXCoord] = useState(0)
+  const [yCoord, setYCoord] = useState(0)
 
   function randomNumber() {
     let randomNum = Math.floor(Math.random() * 1000000)
     setNum(randomNum)
   }
+  
+  const trackMouseMovement = (e) => {
+    let coordX = e.clientX
+    let coordY = e.clientY
+    document.querySelector("#xcoord").innerHTML = coordX
+    document.querySelector("#ycoord").innerHTML = coordY
+  }
+
+  useEffect(() => 
+  document.querySelector(".App").addEventListener("mousemove", 
+  function(e) {
+      trackMouseMovement(e)
+  }))
+
+  
   return (
     <div className="App">
       <h1> HOOKZ </h1>
       <h2>RANDOM NUMBER: {num}</h2>
       <button onClick={() => randomNumber()}>Number Generator</button>
+      <h1>Mouse movement - X:<span id="xcoord"></span>  Y:<span id="ycoord"></span></h1>
+
     </div>
   );
 }
